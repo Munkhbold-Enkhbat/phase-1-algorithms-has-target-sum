7eb48f9f36e3a59540f3f5a66be7a4afcda6f931
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-02-20 21:48:10 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+let hasTargetSum = (array, target) => {
+  // let foundPairs = false
+  for(let i = 0; i < array.length; i++) {
+    for(let j = 0; j < array.length; j++) {
+      if(i !== j && array[i] + array[j] === target){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n) * O(n) + O(n) + O(1) => O(n^2) + O(n) + O(1) => O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  //every integer of array
+  // every integer of other integers in array 
+  //  if they are not same num and their sum is equal to target 
+  //   return true
+  //  else 
+  //   return false
 */
 
 /*
   Add written explanation of your solution here
+  //take every integer of array 
+  //and check with other integers in array
+  //if not same num and their sum is equal to target
+  //return true if yes
+  //otherwise
+  //return false
 */
 
 // You can run `node index.js` to view these console logs
